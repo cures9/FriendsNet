f1b05d77bc28ab4a37aa973eda8c6f8e48c2e6d6
jorge <jorge_92cc@hotmail.es>
2017-08-25 19:57:38 +0200

    swagger de Person y Group funcionan correctamente

--- friendsnet/friendsnet/src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/GroupController.java
+++ friendsnet/friendsnet/src/main/java/com/everis/alicante/courses/beca/summer17/friendsnet/controller/GroupController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ import com.everis.alicante.courses.beca.summer17.friendsnet.entity.Person;
 import com.everis.alicante.courses.beca.summer17.friendsnet.manager.GroupManager;
 
 @RestController
-@RequestMapping
+@RequestMapping("/group")
 @Transactional
 public class GroupController {
 	
@@ -39,8 +41,8 @@ public class GroupController {
 		return manager.save(group);
 	}
 	
-	@GetMapping
-	public Group getByPersonId(Long id) {
+	@GetMapping("/person{id}")
+	public Group getByPersonId(@PathVariable Long id) {
 		return null;
 	}
 	
@@ -49,7 +51,7 @@ public class GroupController {
 		return null;
 	}
 	
-	@PostMapping
+	@DeleteMapping
 	public void remove(@RequestParam Long id) {
 		manager.remove(manager.findById(id));
 	}
